c05527cfba4fa536c9195acd5cf55926aaf4c774
Nabs-1 <nabilah.aralepo@gmail.com>
2025-03-26 16:16:31 -0500

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
 sudo apt update
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63667EE74BBA1F0A08A698725BA31D57EF5975CA
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
+sudo apt install ca-certificates -y
+# Install Java
 sudo apt install openjdk-17-jre-headless -y
-sudo apt install ca-certificates
-sudo apt update
-sudo apt install maven -y
+# Add Jenkins repository
+wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] http://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
 sudo apt install jenkins -y
-sudo apt-get update -y
+# Install Maven
+sudo apt install maven -y
+# Install Terraform
+sudo apt install gpg -y
 wget -O- https://apt.releases.hashicorp.com/gpg | \
     gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install terraform -y
-echo 'clearing screen...' && sleep 5
-clear
-echo 'jenkins is installed'
-echo 'this is the default password :' $(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 # Start Jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+# Display Jenkins initial admin password
+echo 'Jenkins is installed.'
+echo 'This is the default password: ' $(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
